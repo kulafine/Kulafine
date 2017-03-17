1facbce53c43a30562613f434226b3f363d1f09a
Kulafine <kulafine@gmail.com>
2017-03-17 10:06:53 -0700

    beginning

--- app/src/main/java/com/example/prinh/kulafine/MainActivity.java
+++ app/src/main/java/com/example/prinh/kulafine/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.prinh.kulafine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button david,prince;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prince = (Button)findViewById(R.id.button2);
+        david = (Button)findViewById(R.id.button);
+
+        prince.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        david.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Your activities.
+            }
+        });
+
     }
 }
